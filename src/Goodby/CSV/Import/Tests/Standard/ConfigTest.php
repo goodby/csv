a15ac8939ff8aedd4a885467f21f7923a58f69d4
suin <suinyeze@gmail.com>
2012-12-05 16:58:35 +0900

    implement Config class

---
+++ src/Goodby/CSV/Import/Tests/Standard/ConfigTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Goodby\CSV\Import\Tests\Standard;
+
+use Goodby\CSV\Import\Standard\Config;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDelimiter()
+    {
+        $config = new Config();
+        $this->assertSame(',', $config->getDelimiter());
+        $config->setDelimiter('del');
+        $this->assertSame('del', $config->getDelimiter());
+    }
+
+    public function testEnclosure()
+    {
+        $config = new Config();
+        $this->assertSame('"', $config->getEnclosure());
+        $this->assertSame('enc', $config->setEnclosure('enc')->getEnclosure());
+    }
+
+    public function testEscape()
+    {
+        $config = new Config();
+        $this->assertSame('\\', $config->getEscape());
+        $this->assertSame('esc', $config->setEscape('esc')->getEscape());
+    }
+}
