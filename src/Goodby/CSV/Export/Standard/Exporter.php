96298166b69cfe5558606481137fe2b466dc6dcf
suin <suinyeze@gmail.com>
2012-12-06 16:29:54 +0900

    add Exporter class

---
+++ src/Goodby/CSV/Export/Standard/Exporter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Goodby\CSV\Export\Standard;
+
+use Goodby\CSV\Export\Protocol\ExporterInterface;
+use Goodby\CSV\Export\Protocol\Exception\IOException;
+
+class Exporter implements ExporterInterface
+{
+    /**
+     * {@inherit}
+     */
+    public function export($filename, $rows)
+    {
+        // TODO: Implement export() method.
+    }
+}
