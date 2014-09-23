ce16156d11bae4dad72a866dd0ff9de7c10e272d
Ionut Plesca <sirlucas_ro@yahoo.com>
2014-09-23 16:44:54 +0100

    Adding Laadinek' comment fix

--- src/Goodby/CSV/Export/Standard/Exporter.php
+++ src/Goodby/CSV/Export/Standard/Exporter.php
@@ -53,7 +53,7 @@ class Exporter implements ExporterInterface
     {
         $delimiter   = $this->config->getDelimiter();
         $enclosure   = $this->config->getEnclosure();
-        $enclosure   = empty($enclosure) ? "\0" : $enclosure;
+        $enclosure   = "\0" == $enclosure ? "\0" : $enclosure;
         $newline     = $this->config->getNewline();
         $fromCharset = $this->config->getFromCharset();
         $toCharset   = $this->config->getToCharset();
